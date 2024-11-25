8c8055587ff375fe0c7ea86ae94b1e70bda2d311
kamcio2603 <kamcio2603@gmail.com>
2024-11-26 00:05:29 +0100

    fix2

--- app/models/channel/driver/sms_gate.rb
+++ app/models/channel/driver/sms_gate.rb
@@ -1,41 +1,45 @@
-class Channel::Driver::SmsGate
-  NAME = 'sms_gate'.freeze
+module Channel
+  module Driver
+    class SmsGate
+      NAME = 'sms_gate'.freeze
 
-  def self.definition
-    {
-      name: 'SMS Gate',
-      adapter: 'sms_gate',
-      account: [
-        { name: 'api_key', display: 'API Key', tag: 'input', type: 'text', limit: 100, null: false },
-        { name: 'api_url', display: 'API URL', tag: 'input', type: 'text', limit: 100, null: false },
-        { name: 'sender_number', display: 'Sender Number', tag: 'input', type: 'text', limit: 20, null: true }
-      ],
-      notification: [
-        { name: 'options::notification::sender', display: 'Sender', tag: 'input', type: 'text', limit: 20, null: true },
-        { name: 'options::notification::recipient', display: 'Recipient', tag: 'input', type: 'text', limit: 20, null: false },
-      ],
-    }
-  end
+      def self.definition
+        {
+          name: 'SMS Gate',
+          adapter: 'sms_gate',
+          account: [
+            { name: 'api_key', display: 'API Key', tag: 'input', type: 'text', limit: 100, null: false },
+            { name: 'api_url', display: 'API URL', tag: 'input', type: 'text', limit: 100, null: false },
+            { name: 'sender_number', display: 'Sender Number', tag: 'input', type: 'text', limit: 20, null: true }
+          ],
+          notification: [
+            { name: 'options::notification::sender', display: 'Sender', tag: 'input', type: 'text', limit: 20, null: true },
+            { name: 'options::notification::recipient', display: 'Recipient', tag: 'input', type: 'text', limit: 20, null: false },
+          ],
+        }
+      end
 
-  def send(options, attr, notification = false)
-    Rails.logger.debug "Sending SMS via SMS Gate: #{options.inspect}"
+      def send(options, attr, notification = false)
+        Rails.logger.debug "Sending SMS via SMS Gate: #{options.inspect}"
 
-    return false if !options[:recipient]
+        return false if !options[:recipient]
 
-    sms_gate = SmsGate.new(
-      options[:api_key],
-      options[:api_url]
-    )
+        sms_gate = SmsGate.new(
+          options[:api_key],
+          options[:api_url]
+        )
 
-    message = NotificationFactory::Renderer.new(
-      objects: { ticket: options[:ticket] },
-      locale: 'pl-pl'
-    ).render(attr)
+        message = NotificationFactory::Renderer.new(
+          objects: { ticket: options[:ticket] },
+          locale: 'pl-pl'
+        ).render(attr)
 
-    sms_gate.send_sms(
-      to: options[:recipient],
-      message: message,
-      from: options[:sender] || options[:sender_number]
-    )
+        sms_gate.send_sms(
+          to: options[:recipient],
+          message: message,
+          from: options[:sender] || options[:sender_number]
+        )
+      end
+    end
   end
 end
